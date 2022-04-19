4e6456e0bb404041df12fdf551c780532fb94478
Pham Dat <phamthanhdat17092004@gmail.com>
2022-04-19 20:21:57 +0700

    fix lualine

--- lua/lualine/themes/dracula-nvim.lua
+++ lua/lualine/themes/dracula-nvim.lua
@@ -4,13 +4,13 @@ local C = require "nvim-dracula.colors"
 
 dracula.normal = {
   a = { fg = C.black, bg = C.purple, gui = "bold" },
-  b = { fg = C.bright_purple, bg = C.grey, gui = "bold" },
+  b = { fg = C.bright_purple, bg = C.visual, gui = "bold" },
   c = { fg = C.fg, bg = C.selection },
 }
 
 dracula.visual = {
   a = { fg = C.black, bg = C.pink, gui = "bold" },
-  b = { fg = C.pink, bg = C.grey },
+  b = { fg = C.pink, bg = C.visual },
 }
 
 dracula.inactive = {
@@ -20,13 +20,13 @@ dracula.inactive = {
 
 dracula.replace = {
   a = { fg = C.black, bg = C.yellow, gui = "bold" },
-  b = { fg = C.yellow, bg = C.grey },
+  b = { fg = C.yellow, bg = C.visual },
   c = { fg = C.fg, bg = C.selection },
 }
 
 dracula.insert = {
   a = { fg = C.black, bg = C.green, gui = "bold" },
-  b = { fg = C.green, bg = C.grey },
+  b = { fg = C.green, bg = C.visual },
   c = { fg = C.fg, bg = C.selection },
 }
 
