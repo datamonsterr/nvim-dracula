f8aef163baafc789eace1659a210bb7eee0e8b82
Pham Dat <phamthanhdat17092004@gmail.com>
2022-04-19 21:20:07 +0700

    try to disable highlight for unused plugins

--- lua/nvim-dracula/plugins_hl.lua
+++ lua/nvim-dracula/plugins_hl.lua
@@ -2,9 +2,17 @@ local plugins_highlight = {}
 local extend = function(table)
   plugins_highlight = vim.tbl_deep_extend("force", require("nvim-dracula.all_plugins_hl." .. table), plugins_highlight)
 end
+local check_avail = function(plugin)
+  local is_avail, _ = pcall(require, plugin)
+  return is_avail
+end
 
-extend "neo-tree"
-extend "telescope"
+if check_avail "neo-tree.nvim" then
+  extend "neo-tree"
+end
+if check_avail "telescope" then
+  extend "telescope"
+end
 extend "bufferline"
 extend "nvim-web-devicons"
 extend "nvim-notify"
