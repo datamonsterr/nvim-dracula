f0d4340f07fd77e984d846625a6b0690e9f7a8bb
Pham Dat <phamthanhdat17092004@gmail.com>
2022-04-19 16:56:19 +0700

    nothing to describe

--- lua/lualine/themes/dracula-nvim.lua
+++ lua/lualine/themes/dracula-nvim.lua
@@ -1,40 +1,35 @@
 local dracula = {}
 
-local colors = require("nvim-dracula.colors")
+local C = require("nvim-dracula.colors")
 
-local bg = ""
-if vim.g.dracula_lualine_bg_color ~= nil then
-	bg = vim.g.dracula_lualine_bg_color
-else
-	bg = colors.black
-end
+local bg = C.selection
 
 dracula.normal = {
-	a = { fg = colors.black, bg = colors.purple, gui = "bold" },
-	b = { fg = colors.purple, bg = bg },
-	c = { fg = colors.white, bg = bg },
+	a = { fg = C.black, bg = C.purple, gui = "bold" },
+	b = { fg = C.purple, bg = bg },
+	c = { fg = C.white, bg = bg },
 }
 
 dracula.visual = {
-	a = { fg = colors.black, bg = colors.pink, gui = "bold" },
-	b = { fg = colors.pink, bg = bg },
+	a = { fg = C.black, bg = C.pink, gui = "bold" },
+	b = { fg = C.pink, bg = bg },
 }
 
 dracula.inactive = {
-	a = { fg = colors.white, bg = colors.gray, gui = "bold" },
-	b = { fg = colors.black, bg = colors.white },
+	a = { fg = C.white, bg = C.gray, gui = "bold" },
+	b = { fg = C.black, bg = C.white },
 }
 
 dracula.replace = {
-	a = { fg = colors.black, bg = colors.yellow, gui = "bold" },
-	b = { fg = colors.yellow, bg = bg },
-	c = { fg = colors.white, bg = bg },
+	a = { fg = C.black, bg = C.yellow, gui = "bold" },
+	b = { fg = C.yellow, bg = bg },
+	c = { fg = C.white, bg = bg },
 }
 
 dracula.insert = {
-	a = { fg = colors.black, bg = colors.green, gui = "bold" },
-	b = { fg = colors.green, bg = bg },
-	c = { fg = colors.white, bg = bg },
+	a = { fg = C.black, bg = C.green, gui = "bold" },
+	b = { fg = C.green, bg = bg },
+	c = { fg = C.white, bg = bg },
 }
 
 return dracula
--- lua/nvim-dracula/init.lua
+++ lua/nvim-dracula/init.lua
@@ -7,18 +7,11 @@ vim.o.termguicolors = true
 vim.g.colors_name = "nvim-dracula"
 
 C = require("nvim-dracula.colors")
-Config = require("nvim-dracula.config")
 
-local base = require("nvim-dracula.base")
-local treesitter = require("nvim-dracula.treesitter")
-local lsp = require("nvim-dracula.lsp")
-local markdown = require("nvim-dracula.markdown")
-local plugins_hl = require("nvim-dracula.plugins_hl")
-
-local sources = { base, treesitter, lsp, markdown, plugins_hl }
+local sources = { "base", "treesitter", "lsp", "markdown", "plugins_hl" }
 
 for _, source in ipairs(sources) do
-	for group, spec in pairs(require("user.dracula_theme." .. source)) do
+	for group, spec in pairs(require("nvim-dracula." .. source)) do
 		vim.api.nvim_set_hl(0, group, spec)
 	end
 end
