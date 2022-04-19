8928af0f02345894049453ff224dd99cfb4801ae
Pham Dat <phamthanhdat17092004@gmail.com>
2022-04-19 17:37:18 +0700

    add which-key

--- lua/nvim-dracula/all_plugins_hl/which-key.lua
+++ lua/nvim-dracula/all_plugins_hl/which-key.lua
@@ -1,5 +1,6 @@
 return {
-  WhichKeyFloat = { fg = C.bright_purple, bg = C.bg },
+  WhichKeyDesc = { fg = C.bright_purple, bg = C.bg },
+  WhichKeyGroup = { fg = C.bright_purple, bg = C.bg },
   WhichKeySepartor = { fg = C.comment, bg = C.bg },
-  WhichKey = { fg = C.white, bg = C.bg },
+  WhichKey = { fg = C.fg, bg = C.bg },
 }
