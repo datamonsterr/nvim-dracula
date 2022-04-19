21a4d239231fe6e8b5c0acb792ba148dd33a2dad
Pham Dat <phamthanhdat17092004@gmail.com>
2022-04-19 20:20:44 +0700

    fix lualine

--- lua/lualine/themes/dracula-nvim.lua
+++ lua/lualine/themes/dracula-nvim.lua
@@ -2,34 +2,32 @@ local dracula = {}
 
 local C = require "nvim-dracula.colors"
 
-local bg = C.selection
-
 dracula.normal = {
   a = { fg = C.black, bg = C.purple, gui = "bold" },
-  b = { fg = C.purple, bg = bg },
-  c = { fg = C.white, bg = bg },
+  b = { fg = C.bright_purple, bg = C.grey, gui = "bold" },
+  c = { fg = C.fg, bg = C.selection },
 }
 
 dracula.visual = {
   a = { fg = C.black, bg = C.pink, gui = "bold" },
-  b = { fg = C.pink, bg = bg },
+  b = { fg = C.pink, bg = C.grey },
 }
 
 dracula.inactive = {
-  a = { fg = C.white, bg = C.gray, gui = "bold" },
-  b = { fg = C.black, bg = C.white },
+  a = { fg = C.fg, bg = C.gray, gui = "bold" },
+  b = { fg = C.black, bg = C.fg },
 }
 
 dracula.replace = {
   a = { fg = C.black, bg = C.yellow, gui = "bold" },
-  b = { fg = C.yellow, bg = bg },
-  c = { fg = C.white, bg = bg },
+  b = { fg = C.yellow, bg = C.grey },
+  c = { fg = C.fg, bg = C.selection },
 }
 
 dracula.insert = {
   a = { fg = C.black, bg = C.green, gui = "bold" },
-  b = { fg = C.green, bg = bg },
-  c = { fg = C.white, bg = bg },
+  b = { fg = C.green, bg = C.grey },
+  c = { fg = C.fg, bg = C.selection },
 }
 
 return dracula
