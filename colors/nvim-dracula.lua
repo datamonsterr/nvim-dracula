af95dc84f5737bc521cf0aa296c9489b02e13b07
datamonsterr <94211882+datamonsterr@users.noreply.github.com>
2022-04-19 12:33:01 +0700

    Update nvim-dracula.lua

--- colors/nvim-dracula.lua
+++ colors/nvim-dracula.lua
@@ -4,7 +4,7 @@ package.loaded["nvim-dracula.base"] = nil
 package.loaded["nvim-dracula.cmp"] = nil
 package.loaded["nvim-dracula.lsp"] = nil
 package.loaded["nvim-dracula.markdown"] = nil
-package.loaded["nvim-dracula.others"] = nil
+package.loaded["nvim-dracula.plugins_hl"] = nil
 package.loaded["nvim-dracula.treesitter"] = nil
 
 require("nvim-dracula")
