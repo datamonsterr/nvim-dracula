bc6daaf46f94fe68f62733018a8a282d488ad74e
Pham Dat <phamthanhdat17092004@gmail.com>
2022-04-20 22:51:08 +0700

    fix user config

--- lua/nvim-dracula/init.lua
+++ lua/nvim-dracula/init.lua
@@ -11,16 +11,10 @@ function M.apply()
 
   C = require "nvim-dracula.colors"
 
-  local sources = { "base", "treesitter", "lsp", "markdown", "plugins_hl", "diagnostics", "cmp" }
+  local sources = { "user_highlights", "base", "treesitter", "lsp", "markdown", "plugins_hl", "diagnostics", "cmp" }
 
   for _, source in ipairs(sources) do
     for group, spec in pairs(require("nvim-dracula." .. source)) do
-      vim.api.nvim_set_hl(0, group, spec)
-    end
-  end
-
-  if type(vim.g.dracula_highlights) == "table" then
-    for group, spec in pairs(vim.g.dracula_highlights) do
       vim.api.nvim_set_hl(0, group, spec)
     end
   end
---
+++ lua/nvim-dracula/user_highlights.lua
@@ -0,0 +1,5 @@
+if type(vim.g.dracula_highlights) == "table" then
+  return vim.g.dracula_highlights
+else
+  return {}
+end
