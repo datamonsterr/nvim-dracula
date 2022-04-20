6d6a0e2950a57ce06e180d06bd8cf6efefe996d3
Pham Dat <phamthanhdat17092004@gmail.com>
2022-04-20 20:52:54 +0700

    fix user config

--- lua/nvim-dracula/usr_opts.lua
+++ lua/nvim-dracula/usr_opts.lua
@@ -1,6 +1,7 @@
+---@diagnostic disable: lowercase-global
 local M = {}
 
-function M.setup(user_settings)
+function setup(user_settings)
   if type(user_settings) == "table" then
     M.usr_colors = user_settings.colors
     M.usr_highlights = user_settings.highlights
