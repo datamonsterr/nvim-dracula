e59fa555f11eb35319f97eacbd0cd336dc71d3c9
Pham Dat <phamthanhdat17092004@gmail.com>
2022-04-20 21:01:52 +0700

    fix user config

--- lua/nvim-dracula/init.lua
+++ lua/nvim-dracula/init.lua
@@ -1,23 +1,5 @@
 local M = {}
 
-vim.cmd "hi clear"
-if vim.fn.exists "syntax_on" then
-  vim.cmd "syntax reset"
-end
-vim.o.background = "dark"
-vim.o.termguicolors = true
-vim.g.colors_name = "dracula"
-
-C = require "nvim-dracula.colors"
-
-local sources = { "base", "treesitter", "lsp", "markdown", "plugins_hl", "diagnostics", "cmp" }
-
-for _, source in ipairs(sources) do
-  for group, spec in pairs(require("nvim-dracula." .. source)) do
-    vim.api.nvim_set_hl(0, group, spec)
-  end
-end
-
 function M.setup(user_settings)
   if type(user_settings) == "table" then
     M.usr_colors = user_settings.colors
@@ -27,9 +9,28 @@ function M.setup(user_settings)
   end
 end
 
-if require("nvim-dracula").usr_highlights then
-  for group, spec in pairs(require("nvim-dracula").usr_highlights) do
-    vim.api.nvim_set_hl(0, group, spec)
+function M.apply()
+  vim.cmd "hi clear"
+  if vim.fn.exists "syntax_on" then
+    vim.cmd "syntax reset"
+  end
+  vim.o.background = "dark"
+  vim.o.termguicolors = true
+  vim.g.colors_name = "dracula"
+
+  C = require "nvim-dracula.colors"
+
+  local sources = { "base", "treesitter", "lsp", "markdown", "plugins_hl", "diagnostics", "cmp" }
+
+  for _, source in ipairs(sources) do
+    for group, spec in pairs(require("nvim-dracula." .. source)) do
+      vim.api.nvim_set_hl(0, group, spec)
+    end
+  end
+  if require("nvim-dracula").usr_highlights then
+    for group, spec in pairs(require("nvim-dracula").usr_highlights) do
+      vim.api.nvim_set_hl(0, group, spec)
+    end
   end
 end
 
