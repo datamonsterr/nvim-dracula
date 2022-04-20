f7b39a2e469d54f05daacb5f6535685bf0356a29
Pham Dat <phamthanhdat17092004@gmail.com>
2022-04-20 18:48:19 +0700

    fix whichkey

--- lua/nvim-dracula/all_plugins_hl/which-key.lua
+++ lua/nvim-dracula/all_plugins_hl/which-key.lua
@@ -1,6 +1,6 @@
 return {
   WhichKeyDesc = { fg = C.bright_purple, bg = C.bg },
   WhichKeyGroup = { fg = C.bright_purple, bg = C.bg },
-  WhichKeySepartor = { fg = C.comment, bg = C.bg },
+  WhichKeySeperator = { fg = C.comment, bg = C.bg },
   WhichKey = { fg = C.fg, bg = C.bg },
 }
