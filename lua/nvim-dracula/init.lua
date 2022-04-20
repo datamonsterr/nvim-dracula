5a53c673890671c7420f12c556e5b2d35f85b6da
Pham Dat <phamthanhdat17092004@gmail.com>
2022-04-20 20:59:49 +0700

    fix user config

--- lua/nvim-dracula/init.lua
+++ lua/nvim-dracula/init.lua
@@ -1,3 +1,5 @@
+local M = {}
+
 vim.cmd "hi clear"
 if vim.fn.exists "syntax_on" then
   vim.cmd "syntax reset"
@@ -16,8 +18,19 @@ for _, source in ipairs(sources) do
   end
 end
 
-if require("nvim-dracula.usr_opts").usr_highlights then
-  for group, spec in pairs(require("nvim-dracula.usr_opts").usr_highlights) do
+function M.setup(user_settings)
+  if type(user_settings) == "table" then
+    M.usr_colors = user_settings.colors
+    M.usr_highlights = user_settings.highlights
+  else
+    vim.notify("setup needs a table value", "error", "Dracula")
+  end
+end
+
+if require("nvim-dracula").usr_highlights then
+  for group, spec in pairs(require("nvim-dracula").usr_highlights) do
     vim.api.nvim_set_hl(0, group, spec)
   end
 end
+
+return M
