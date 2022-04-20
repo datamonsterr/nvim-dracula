469e5a1b2763e649cd8f733cc725dc6c7ea83a95
Pham Dat <phamthanhdat17092004@gmail.com>
2022-04-20 21:04:56 +0700

    fix user config

--- lua/nvim-dracula/init.lua
+++ lua/nvim-dracula/init.lua
@@ -1,12 +1,8 @@
 local M = {}
 
 function M.setup(user_settings)
-  if type(user_settings) == "table" then
-    M.usr_colors = user_settings.colors
-    M.usr_highlights = user_settings.highlights
-  else
-    vim.notify("setup needs a table value", "error", "Dracula")
-  end
+  M.usr_colors = user_settings.colors
+  M.usr_highlights = user_settings.highlights
 end
 
 function M.apply()
