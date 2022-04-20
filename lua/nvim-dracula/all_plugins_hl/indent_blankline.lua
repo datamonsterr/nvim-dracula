15bd990cb3500de19125199217fd636297dca029
Pham Dat <phamthanhdat17092004@gmail.com>
2022-04-20 20:28:52 +0700

    fix indent-blankline

--- lua/nvim-dracula/all_plugins_hl/indent_blankline.lua
+++ lua/nvim-dracula/all_plugins_hl/indent_blankline.lua
@@ -1,7 +1,7 @@
 return {
-  IndentBlanklineContextChar = { fg = C.comment, bold = true },
+  IndentBlanklineContextChar = { fg = C.bright_cyan, bold = true },
   IndentBlanklineSpaceChar = { fg = C.grey, nocombine = true },
-  IndentBlanklineChar = { fg = C.grey, nocombine = true },
-  IndentBlanklineContextStart = { fg = C.grey, underline = true },
+  IndentBlanklineChar = { fg = C.comment, nocombine = true },
+  IndentBlanklineContextStart = { fg = C.comment, underline = true },
   IndentBlanklineSpaceCharBlankline = { fg = C.grey, nocombine = true },
 }
