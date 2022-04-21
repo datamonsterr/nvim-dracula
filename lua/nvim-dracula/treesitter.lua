7a2b852d0f87ea7728aae9a1acb3a11d9061501c
Pham Dat <phamthanhdat17092004@gmail.com>
2022-04-21 23:30:56 +0700

    fix treesitter

--- lua/nvim-dracula/treesitter.lua
+++ lua/nvim-dracula/treesitter.lua
@@ -32,7 +32,7 @@ return {
   TSParameter = { fg = C.orange },
   TSParameterReference = { fg = C.orange },
   TSMethod = { fg = C.green },
-  TSProperty = { fg = C.cyan },
+  TSProperty = { fg = C.green },
   TSLabel = { fg = C.cyan },
   TSOperator = { fg = C.pink },
   TSException = { fg = C.purple },
