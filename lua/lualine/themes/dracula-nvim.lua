a92c31bdffaa0bac92d6576711e08fc34b0d6a04
Pham Dat <phamthanhdat17092004@gmail.com>
2022-04-21 18:15:22 +0700

    fix user config

--- lua/lualine/themes/dracula-nvim.lua
+++ lua/lualine/themes/dracula-nvim.lua
@@ -5,7 +5,7 @@ local C = require "nvim-dracula.colors"
 dracula.normal = {
   a = { fg = C.black, bg = C.purple, gui = "bold" },
   b = { fg = C.bright_purple, bg = C.visual, gui = "bold" },
-  c = { fg = C.comment, bg = C.selection },
+  c = { fg = C.fg, bg = C.selection },
 }
 
 dracula.visual = {
@@ -14,20 +14,20 @@ dracula.visual = {
 }
 
 dracula.inactive = {
-  a = { fg = C.comment, bg = C.gray, gui = "bold" },
-  b = { fg = C.black, bg = C.comment },
+  a = { fg = C.fg, bg = C.gray, gui = "bold" },
+  b = { fg = C.black, bg = C.fg },
 }
 
 dracula.replace = {
   a = { fg = C.black, bg = C.yellow, gui = "bold" },
   b = { fg = C.yellow, bg = C.visual },
-  c = { fg = C.comment, bg = C.selection },
+  c = { fg = C.fg, bg = C.selection },
 }
 
 dracula.insert = {
   a = { fg = C.black, bg = C.green, gui = "bold" },
   b = { fg = C.green, bg = C.visual },
-  c = { fg = C.comment, bg = C.selection },
+  c = { fg = C.fg, bg = C.selection },
 }
 
 return dracula
