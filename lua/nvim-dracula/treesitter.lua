c97545addea0ec7a3c8eb3bed6158dcb963d770f
Pham Dat <phamthanhdat17092004@gmail.com>
2022-04-22 10:44:46 +0700

    fix treesitter

--- lua/nvim-dracula/treesitter.lua
+++ lua/nvim-dracula/treesitter.lua
@@ -54,7 +54,7 @@ return {
   rainbowcol2 = { bold = true, fg = C.bright_cyan },
   rainbowcol3 = { bold = true, fg = C.bright_purple },
   rainbowcol4 = { bold = true, fg = C.bright_green },
-  rainbowcol5 = { bold = true, fg = C.bright_magenta },
-  rainbowcol6 = { bold = true, fg = C.bright_cyan },
-  rainbowcol7 = { bold = true, fg = C.bright_purple },
+  rainbowcol5 = { bold = true, fg = C.bright_yellow },
+  rainbowcol6 = { bold = true, fg = C.bright_magenta },
+  rainbowcol7 = { bold = true, fg = C.bright_cyan },
 }
