0ea6081b43cf7f0a1e86d0d941e8a83f5254a9c9
Pham Dat <phamthanhdat17092004@gmail.com>
2022-04-24 13:09:45 +0700

    fix highlights

--- lua/nvim-dracula/css.lua
+++ lua/nvim-dracula/css.lua
@@ -1,9 +1,11 @@
 return {
-  cssClassName = { fg = C.green, bold = true },
-  cssClassNameDot = { fg = C.green, bold = true },
+  cssClassName = { fg = C.bright_cyan, bold = true },
+  cssClassNameDot = { fg = C.bright_cyan, bold = true },
   cssIndentifier = { fg = C.cyan, bold = true },
   cssKeyFrameProp = { fg = C.pink },
   cssFunction = { fg = C.bright_magenta },
   cssProp = { fg = C.bright_purple },
   cssColor = { fg = C.fg },
+  cssFunctionName = { fg = C.green, bold = true },
+  cssAtRule = { fg = C.pink, bold = true },
 }
