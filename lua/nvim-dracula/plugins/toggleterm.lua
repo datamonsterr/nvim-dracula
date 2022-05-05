5ba01162d94410e110b18872ad02447d72f83ba5
Pham Dat <phamthanhdat17092004@gmail.com>
2022-05-05 20:34:48 +0700

    fsadf

--- lua/nvim-dracula/plugins/toggleterm.lua
+++ lua/nvim-dracula/plugins/toggleterm.lua
@@ -3,9 +3,9 @@ local toggleterm = {
 }
 
 for i in 1, 20 do
-  toggleterm["ToggleTerm" .. i .. "Normal"] = { link = "Normal" }
-  toggleterm["ToggleTerm" .. i .. "NormalFloat"] = { link = "Normal" }
-  toggleterm["ToggleTerm" .. i .. "FloatBorder"] = { link = "Normal" }
+  toggleterm["ToggleTerm" .. tostring(i) .. "Normal"] = { link = "Normal" }
+  toggleterm["ToggleTerm" .. tostring(i) .. "NormalFloat"] = { link = "Normal" }
+  toggleterm["ToggleTerm" .. tostring(i) .. "FloatBorder"] = { link = "Normal" }
 end
 
 return toggleterm
