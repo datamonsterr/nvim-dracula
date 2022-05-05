8b7cd5ec54b300d20680de42a973d5abf49fe5b5
Pham Dat <phamthanhdat17092004@gmail.com>
2022-05-05 09:55:51 +0700

    fsadf

--- lua/nvim-dracula/plugins/which-key.lua
+++ lua/nvim-dracula/plugins/which-key.lua
@@ -1,6 +1,6 @@
 return {
-  WhichKeyDesc = { fg = C.bright_purple, bg = C.bg },
-  WhichKeyGroup = { fg = C.bright_purple, bg = C.bg },
-  WhichKeySeperator = { fg = C.comment, bg = C.bg },
-  WhichKey = { fg = C.fg, bg = C.bg },
+  WhichKeyDesc = { fg = C.bright_purple },
+  WhichKeyGroup = { fg = C.bright_purple },
+  WhichKeySeperator = { fg = C.comment },
+  WhichKey = { fg = C.fg },
 }
