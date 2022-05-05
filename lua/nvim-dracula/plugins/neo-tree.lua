8e9ba7d2c4cf6d3dd23e5da0fe00769d520aea1c
Pham Dat <phamthanhdat17092004@gmail.com>
2022-05-05 10:10:32 +0700

    fsadf

--- lua/nvim-dracula/plugins/neo-tree.lua
+++ lua/nvim-dracula/plugins/neo-tree.lua
@@ -11,6 +11,6 @@ return {
   NeoTreeGitModified = { fg = C.orange },
   NeoTreeGitUntracked = { fg = C.yellow },
   NeoTreeNormal = { bg = C.menu, fg = C.fg },
-  NeoTreeNormalNC = { fg = C.bright_purple, bg = C.menu, bold = true },
+  NeoTreeNormalNC = { fg = C.bright_purple, bg = C.menu },
   NeoTreeSymbolicLinkTarget = { fg = C.cyan },
 }
