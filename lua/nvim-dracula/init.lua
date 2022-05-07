6c9db841e7a14cc91f6b8fb5857d0efe8eb217fc
Pham Dat <phamthanhdat17092004@gmail.com>
2022-05-07 18:51:51 +0700

    lazyload is hard

--- lua/nvim-dracula/init.lua
+++ lua/nvim-dracula/init.lua
@@ -1,23 +1,17 @@
-local M = {}
+vim.cmd "hi clear"
+if vim.fn.exists "syntax_on" then
+  vim.cmd "syntax reset"
+end
+vim.o.background = "dark"
+vim.o.termguicolors = true
+vim.g.colors_name = "dracula"
 
-function M.apply()
-  vim.cmd "hi clear"
-  if vim.fn.exists "syntax_on" then
-    vim.cmd "syntax reset"
-  end
-  vim.o.background = "dark"
-  vim.o.termguicolors = true
-  vim.g.colors_name = "dracula"
+C = require "nvim-dracula.colors"
 
-  C = require "nvim-dracula.colors"
+local sources = { "css", "base", "treesitter", "lsp", "markdown", "plugins_hl", "diagnostics" }
 
-  local sources = { "css", "base", "treesitter", "lsp", "markdown", "plugins_hl", "diagnostics" }
-
-  for _, source in ipairs(sources) do
-    for group, spec in pairs(require("nvim-dracula." .. source)) do
-      vim.api.nvim_set_hl(0, group, spec)
-    end
+for _, source in ipairs(sources) do
+  for group, spec in pairs(require("nvim-dracula." .. source)) do
+    vim.api.nvim_set_hl(0, group, spec)
   end
 end
-
-return M
