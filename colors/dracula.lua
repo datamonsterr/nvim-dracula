4ac7c6c662245e0f8e3d9edf0cedcc0730248a2a
Pham Dat <phamthanhdat17092004@gmail.com>
2022-05-21 20:45:48 +0700

    update my config

--- colors/dracula.lua
+++ colors/dracula.lua
@@ -1,9 +1,18 @@
 package.loaded["nvim-dracula"] = nil
+package.loaded["nvim-dracula.css"] = nil
+package.loaded["nvim-dracula.diagnostics"] = nil
 package.loaded["nvim-dracula.base"] = nil
-package.loaded["nvim-dracula.cmp"] = nil
 package.loaded["nvim-dracula.lsp"] = nil
 package.loaded["nvim-dracula.markdown"] = nil
 package.loaded["nvim-dracula.plugins_hl"] = nil
 package.loaded["nvim-dracula.treesitter"] = nil
 
 require "nvim-dracula"
+
+-- Support Feline
+local feline_avail, feline = pcall(require, "feline")
+if feline_avail then
+  local feline_theme = require "nvim-dracula.colors"
+  -- feline_theme.bg = feline_theme.menu
+  feline.add_theme("dracula", feline_theme)
+end
