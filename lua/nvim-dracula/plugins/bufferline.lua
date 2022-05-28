7a63388496bb318dda0f2ed1db85539306fb0996
Pham Dat <phamthanhdat17092004@gmail.com>
2022-05-29 06:52:08 +0700

    change bufferline color

--- lua/nvim-dracula/plugins/bufferline.lua
+++ lua/nvim-dracula/plugins/bufferline.lua
@@ -2,7 +2,7 @@ return {
   BufferLineIndicatorSelected = { fg = C.bright_purple, bg = C.bg },
   BufferLineFill = { bg = C.selection },
   BufferLineBufferSelected = { fg = C.bright_purple, bg = C.bg, bold = true },
-  BufferLineBackground = { bg = C.selection, fg = C.dim_purple },
+  BufferLineBackground = { bg = C.visual, fg = C.dim_purple },
   BufferLineSeparator = { bg = C.selection, fg = C.selection },
   BufferLineSeparatorSelected = { bg = C.bg, fg = C.bg },
   BufferLineCloseButton = { bg = C.selection },
