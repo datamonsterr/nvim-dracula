5c05b765c000be053533b2d324e652142772db7c
Pham Dat <phamthanhdat17092004@gmail.com>
2022-05-28 16:18:41 +0700

    dracula doesn't like to be scheduled

--- lua/nvim-dracula/init.lua
+++ lua/nvim-dracula/init.lua
@@ -10,10 +10,8 @@ C = require "nvim-dracula.colors"
 
 local sources = { "css", "base", "treesitter", "lsp", "markdown", "plugins_hl", "diagnostics" }
 
-vim.schedule(function ()
-  for _, source in ipairs(sources) do
-    for group, spec in pairs(require("nvim-dracula." .. source)) do
-      vim.api.nvim_set_hl(0, group, spec)
-    end
+for _, source in ipairs(sources) do
+  for group, spec in pairs(require("nvim-dracula." .. source)) do
+    vim.api.nvim_set_hl(0, group, spec)
   end
-end)
+end
